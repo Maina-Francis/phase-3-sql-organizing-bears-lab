a702e35f107ddcbe905c21c9e1d9bfd6904b634f
Francis Maina <sebastianfrancix@gmail.com>
2022-10-28 04:59:21 +0300

    Completed assignment

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES ("Mr. Chocolate", 30, "M", "White", NULL, 1),
+("Rowdy", 20, "M", "White", NULL, 1),
+("Tabitha", 34, "F", "White", NULL, 1),
+("Sergeant Brown", 27, "M", "White", NULL, 1),
+("Melissa", 38, "F", "White", NULL, 1),
+("Grinch", 60, "M", "White", NULL, 0),
+("Wendy", 10, "F", "White", NULL, 1),
+(NULL, 40, "F", "White", NULL, 0);
